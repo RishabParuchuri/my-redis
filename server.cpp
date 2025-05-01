b0928986fc73a7dc1e9bb55bad3612ddbb858bb6
Rishab Paruchuri <rishparu@gmail.com>
2025-04-30 23:57:59 -0400

    Finished basic server - listens to connections with sockets

---
+++ server.cpp
@@ -0,0 +1,78 @@
+#include <assert.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+static void die(const char *msg)
+{
+    fprintf(stderr, "[%d] %s\n", errno, msg);
+    abort();
+}
+
+static void test_connection(int connfd)
+{
+    char rbuf[64] = {};
+    ssize_t n = recv(connfd, rbuf, sizeof(rbuf) - 1, 0);
+    if (n < 0)
+    {
+        die("recv() error");
+        return;
+    }
+    printf("client says: %s\n", rbuf);
+
+    char wbuf[] = "hi back to you!!!";
+    send(connfd, wbuf, strlen(wbuf), 0);
+    return;
+}
+
+int main()
+{
+    int sockfd = socket(AF_INET6, SOCK_STREAM, 0);
+    if (sockfd < 0)
+    {
+        die("server socket()");
+    }
+
+    int val = 1;
+    setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
+
+    struct sockaddr_in6 addr = {};
+    addr.sin6_family = AF_INET6;
+    addr.sin6_port = htons(1234);
+    addr.sin6_addr = in6addr_any;
+
+    // binding reserves an ip address and port on computer for the socket
+    int err = bind(sockfd, (const struct sockaddr *)&addr, sizeof(addr));
+    if (err)
+    {
+        die("bind()");
+    }
+
+    // listening tells the kernal that the socket can accept and queue incoming connections.
+    // SOMAXCONN is 4096 connections in queue
+    err = listen(sockfd, SOMAXCONN);
+    if (err)
+    {
+        die("listen()");
+    }
+
+    while (true)
+    {
+        // accept connections
+        struct sockaddr_in6 client_addr = {};
+        socklen_t addrlen = sizeof(client_addr);
+        int connfd = accept(sockfd, (struct sockaddr *)&client_addr, &addrlen);
+        if (connfd < 0)
+        {
+            continue;
+        }
+
+        // implement business logic
+        test_connection(connfd);
+        close(connfd);
+    }
+}
